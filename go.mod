09d30714af957b061091468fb4b1aa5d9928b3a0
Murat Odunc <murat.asya@gmail.com>
2019-01-13 23:00:23 +0300

    changes to try gRPC

--- go.mod
+++ go.mod
@@ -1,6 +1,9 @@
 module shellimsi/hub
 
 require (
-	github.com/shellimsi/proto v0.0.1
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/satori/go.uuid v1.2.0
+	github.com/shellimsi/proto v0.0.4
 	google.golang.org/grpc v1.17.0
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -13,6 +13,13 @@ type Server struct {
 
 // SayHello implements helloworld.GreeterServer
 func (s *Server) Resize(ctx context.Context, in *hub.WinsizeRequest) (*hub.WinsizeResponse, error) {
-	ok := status("OK")
-	return &hub.WinsizeResponse{Status: ok}, nil
+	return &hub.WinsizeResponse{Status: hub.Status_OK}, nil
+}
+
+func (s *Server) Register(ctx context.Context, register *hub.RegisterRequest) (*hub.RegisterResponse, error) {
+	return &hub.RegisterResponse{
+		Host:      "localhost",
+		Port:      8080,
+		SessionId: 1,
+	}, nil
 }
